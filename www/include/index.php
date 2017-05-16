3ea73449733f9bcee70c005ea28fa5cc586c3d26
Khonko Alex <alkhonko@gmail.com>
2017-05-16 13:06:54 +0300

    first delete

--- www/include/index.php
+++ www/include/index.php
@@ -1,6 +1,17 @@
-<!--         -->
+<!--      -->
+
 <?php
-	include ("include/dbConnect.php");
+	require_once ("dbConnect.php");
+?>
+
+<?php
+
+
+
+
+
+
+
 ?>
 
 
@@ -8,77 +19,32 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>SiteName</title>
-	<link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/style.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<!--  1)  добавить метатеги и фавикон   -->
+	<title>our site</title>
+	<link rel="stylesheet" href="../css/reset.css">
+	<link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 	<div id="wrapper">
-		
-			<header>
-				<div id="logo">
-					<a href="index.php"><img src="img/Logo.jpg" title="Главная"></a>
-				</div>
-				<div id="topNav">
-					<ul>
-						<li><a href="include/page1.php">page</a></li>
-						<li><a href="include/page2.php">page</a></li>
-						<li><a href="include/page3.php">page</a></li>
-						<li><a href="include/page4.php">page</a></li>
-					</ul>
-				</div>
-				<div id="botNav">
-					<ul>
-						<li><a href="include/page5.php">page</a></li>
-						<li><a href="include/page6.php">page</a></li>
-						<li><a href="include/page7.php">page</a></li>
-					</ul>
-				</div>
-				<div id="socials">
-				
-				</div>
-			</header>
-		<aside>
+		<div id="header">
+			<?php
+
+			require_once ("blockHeader.php");
 
 
+			?>	
+		</div>
+		<div id="content">
+			<?php
 
+			require_once ("blockContentMain.php")
 
-<?php
-
-
-$sql  = "SELECT * FROM first";
-$result=mysqli_query($link,$sql);
-
-
- if (mysqli_num_rows($result) > 0)
- {
- 	$row=mysqli_fetch_array($result);
- 	$qq=$row["id"];
- }
-
-
- do {
- 	echo '
-
-<ul id="newContent">
-<li style="background:url(img/'.$row["background"].') center " ><a href="include/'.$row["way"].'?'.$row["id"].'" class="input1" >'.$row["name"].'.'.$row["id"].'</a></li>
-</ul>
-
-
-
-';
-
-} 
-while ( $row=mysqli_fetch_array($result));
-return ("$qq")
-?>
-
-
-			</aside>
-		
-			
+			?>
+		</div>
 	</div>
+
+
+
+
+
 </body>
 </html>
